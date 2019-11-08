ebdfe9993111db48bef387ebd56e2ee664637a2d
ericbanker12 <ericbanker12@gmail.com>
2019-11-08 11:00:21 -0600

    move server.js, create resources-router, add GET to receive array of resources, add POST to create a resource

---
+++ api/resources/resources-router.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+
+const resourcesDB = require('./resources-model')
+
+router.get('/', (req, res) => {
+    resourcesDB.get()
+    .then(resp => {
+        res.json(resp)
+    })
+    .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
+
+router.post('/', (req, res) => {
+    if (!req.body) return res.status(400).json({message: 'missing data body'})
+    
+    if (!req.body.name) return res.status(400).json({message: 'missing required field: name'})
+    
+    resourcesDB.add(req.body)
+    .then(resp => {
+        res.json(resp)
+    })
+    .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const helmet = require('helmet')
 
+const resourcesRouter = require('./resources/resources-router')
+
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
+
+server.use('/api/resources', resourcesRouter)
 
 module.exports = server
