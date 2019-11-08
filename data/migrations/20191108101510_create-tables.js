58c1cb8b0b91e2d5b841fcd88435e6a0837ba654
ericbanker12 <ericbanker12@gmail.com>
2019-11-08 10:26:07 -0600

    create migration to create table

---
+++ data/migrations/20191108101510_create-tables.js
@@ -0,0 +1,72 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    // projects
+    .createTable('projects', tbl => {
+        // id primary key
+        tbl.increments()
+        // name string required
+        tbl.string('name').notNullable()
+        // description string
+        tbl.string('description')
+        // completed boolean default false
+        tbl.string('completed').defaultTo(false).notNullable()
+    })
+
+    // tasks
+    .createTable('tasks', tbl => {
+        // id primary key
+        tbl.increments()
+        // description string required
+        tbl.string('description').notNullable()
+        // notes string 
+        tbl.string('notes')
+        // completed boolean default false
+        tbl.string('completed').defaultTo(false).notNullable()
+        // project_id foreign key
+        tbl.integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+    })
+
+    // resources
+    .createTable('resources', tbl => {
+        // id primary key
+        tbl.increments()
+        // name string required unique
+        tbl.string('name').unique().notNullable()
+        // description string
+        tbl.string('description')
+    })
+
+    // project_recources
+    .createTable('project_recources', tbl => {
+        // id primary key
+        tbl.increments()
+        // project_id foreign key
+        tbl.integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        // resource_id foreign key
+        tbl.integer('resource_id')
+            .unsigned()
+            .references('id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('project_recources')
+    .dropTable('resources')
+    .dropTable('tasks')
+    .dropTable('projects')
+};
